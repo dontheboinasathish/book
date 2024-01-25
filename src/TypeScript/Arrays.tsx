a88e5935b1ccefc3d716ce33d5dbbc16298a7aa3
dontheboinasathish <sathishdonthuboina311@gmail.com>
2024-01-25 18:18:24 +0530

    hello chenges

--- src/TypeScript/Arrays.tsx
+++ src/TypeScript/Arrays.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-function Arrays({title}:{title:string}) {
-  return (
-    <div>
-      <button title='iam the bbtton'>Click Me</button>
-      <h1>{title}</h1>
-      <h2>hello </h2>
-    </div>
-  )
-}
-export default Arrays;
+// import React from 'react'
+// function Arrays({title}:{title:string}) {
+//   return (
+//     <div>
+//       <button title='iam the bbtton'>Click Me</button>
+//       <h1>{title}</h1>
+//       <h2>hello </h2>
+//     </div>
+//   )
+// }
+// export default Arrays;
